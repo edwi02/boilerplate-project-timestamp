b66749dcaffeca9d78b3243ea97c4ad35520bdc2
edwi02 <32597410+edwi02@users.noreply.github.com>
2021-06-08 21:06:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -12,6 +13,9 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: "false" }));
+app.use(bodyParser.json());
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -24,9 +28,57 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res)=> {
+  
+  const date = req.params.date;
+  
+  let unix;
+  let utcDate;
+  let result = false;
+  
+  try{
+    
+    let numberRegex = /\d{13}/ig;
+    let isNumber = numberRegex.test(date);
+    
+    let dateRegex = /\d{4}[-]\d{2}[-]\d{1,2}/ig; 
+    let isDate = dateRegex.test(date);
+
+    
+    if( isNumber  ) {
+      unix = date;
+      utcDate = new Date(parseInt(date));
+      console.log('Number', date);
+    } else if( isDate ) {
+      unix = Date.parse(date);
+      utcDate = new Date(date)
+      console.log('Date', date.length);
+
+    } else {
+      return res.json({
+       error : "Invalid Date"
+      })
+    }
+    
+    const utc = utcDate.toUTCString();
+    return res.json({
+      unix,
+      utc
+    })
+  } catch(err) {
+    console.log("err", err);
+    res.json({
+     error : "Invalid Date"
+    })
+  }
+  
+  
+});
+
 
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
